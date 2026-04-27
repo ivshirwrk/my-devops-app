e5809e9d4ae1bc686ae65a2b73bddc628e1e2a24
0936EK <ivshirwrk@gmail.com>
2026-04-27 10:26:12 +0500

    First commit: Flask + Redis + Nginx app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import redis
+import os
+
+app = Flask(__name__)
+r = redis.Redis(host='redis', port=6379, decode_responses=True)
+
+@app.route('/')
+def hello():
+    count = r.incr('visits')
+    return f'Привет, DevOps! Эту страницу посетили {count} раз(а).'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
