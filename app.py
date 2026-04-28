ca178a6b2035848b5dfee1978a9a99919d39faaa
0936EK <ivshirwrk@gmail.com>
2026-04-28 22:01:14 +0500

    Add prometheus metrics exporter

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 import redis
-import os
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+# Статическая метрика о количестве запросов
+metrics.info('app_info', 'Application info', version='1.0.0')
+
 r = redis.Redis(host='redis', port=6379, decode_responses=True)
 
 @app.route('/')
 def hello():
     count = r.incr('visits')
     return f'Привет, DevOps! Эту страницу посетили {count} раз(а).'
+
+# Эндпоинт /metrics создаётся автоматически
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
